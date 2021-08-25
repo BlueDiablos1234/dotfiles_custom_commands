87701b738edbba014d5f8d6ff7611a60eacde2d0
BlueDiablos1234 <57236693+BlueDiablos1234@users.noreply.github.com>
2021-08-25 13:14:04 +0200

    upload V-0.2
    add:
    source ~/.custom 
    in ~/.bashrc

---
+++ .custom.sh
@@ -0,0 +1,13 @@
+# Custom Commands
+alias cowtime='cowsay $(date) | lolcat'
+alias comedit='nano ~/.bashrc'
+alias VC='cd Documents/VC_YR2'
+alias custom='cat commands | lolcat'
+alias custedit='nano commands'
+alias i3edit='nano ~/.config/i3/config file'
+
+# Cpu Commands
+alias speed='cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_max_freq'
+alias cpu-1.6='sudo /bin/bash cpu-1.6'
+alias cpu-2.0='sudo /bin/bash cpu-2.0'
+alias cpu-2.4='sudo /bin/bash cpu-2.4'
