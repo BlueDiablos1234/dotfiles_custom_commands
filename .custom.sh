0374e8afed3322640d4786edc33a594a0fb341aa
Nathan <57236693+sudo-nathan@users.noreply.github.com>
2021-11-22 19:27:27 +0200

    Removed CPU section

--- .custom.sh
+++ .custom.sh
@@ -8,6 +8,3 @@ alias i3edit='nano ~/.config/i3/config file'
 
 # Cpu Commands
 alias speed='cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_max_freq'
-alias cpu-1.6='sudo /bin/bash cpu-1.6'
-alias cpu-2.0='sudo /bin/bash cpu-2.0'
-alias cpu-2.4='sudo /bin/bash cpu-2.4'
