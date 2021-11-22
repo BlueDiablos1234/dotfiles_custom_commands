24d2197376160a0f723188f1ce8544465bd71eaa
sudo-nathan <natengel35@gmail.com>
2021-11-22 20:09:24 +0200

    First commit from CR620

--- .custom.sh
+++ .custom.sh
@@ -1,10 +1,15 @@
 # Custom Commands
 alias cowtime='cowsay $(date) | lolcat'
-alias comedit='nano ~/.bashrc'
-alias VC='cd Documents/VC_YR2'
-alias custom='cat commands | lolcat'
-alias custedit='nano commands'
+alias bashedit='nano ~/.bashrc'
+alias VC='cd; cd Documents/VC'
+alias custom='cat ~/Github/dotfiles_custom_commands/.commands | lolcat'
+alias custedit='nano ~/Github/dotfiles_custom_commands/.commands'
+alias comedit='nano ~/Github/dotfiles_custom_commands/.custom.sh'
 alias i3edit='nano ~/.config/i3/config file'
+alias now='timedatectl'
+alias dotfiles='cd ~/Github/dotfiles_custom_commands'
+alias resolution='arandr'
+alias hack='feh --bg-scale Pictures/Wallpaper/hack.jpg'
 
 # Cpu Commands
 alias speed='cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_max_freq'
