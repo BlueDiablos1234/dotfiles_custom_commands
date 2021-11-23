d32d6aee5d846bea0cda79bbedbc3ec155dee02a
sudo-nathan <natengel35@gmail.com>
2021-11-24 01:08:40 +0200

    finalizing cleanup

--- .pi_bash.sh
+++ .pi_bash.sh
@@ -1,6 +1,6 @@
-alias comedit='cd;nano ~/.pi_bash.sh' #Edits THIS file
-alias custedit='cd;nano ~/.custom' #Edits txt file
-alias custom='cd;figlet Custom | lolcat;cat ~/.custom | lolcat'
+alias comedit='cd;nano ~/Github/dotfiles_rpi/.pi_bash.sh' #Edits THIS file
+alias custedit='cd;nano ~/Github/dotfiles_rpi/.commands' #Edits txt file
+alias custom='cd;figlet Custom | lolcat;cat ~/Github/dotfiles_rpi/.commands | lolcat'
 alias temp='vcgencmd measure_temp | lolcat'
 alias speed='vcgencmd measure_clock arm | lolcat'
 alias info='vcgencmd measure_clock arm | lolcat; vcgencmd measure_temp | lolcat'
